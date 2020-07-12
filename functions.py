89b01dc146cb2a0f4f8bd125fa457f631a52e472
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2020-07-12 20:55:21 +0200

    First Commit

---
+++ functions.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen, Request
+import re
+
+# Definimos la función para eliminar las etiquetas html
+def remove_tags(text):
+    TAG_RE = re.compile(r'<[^>]+>')
+    return TAG_RE.sub('', str(text)).strip()
+
+def obtain_html(url):
+    request = Request(url)
+    response = urlopen(request)
+    html = response.read()
+    html.decode('utf-8')
+    return html
+
+def obtain_page(words, level, index):
+    result = []
+    for word in words:
+        element = []
+
+        # Identificador de nivel e índice
+        element.append('JLPT-N' + str(level))
+        element.append(index)
+        index = index + 1
+
+        # Obtenemos el kanji
+        kanji = word.find('span', {'class': 'text'})
+        kanji = remove_tags(kanji)
+        element.append(kanji)
+
+        # Obtenemos el furigana
+        furigana = word.find('span', {'class': 'furigana'})
+        furigana = remove_tags(furigana)
+        element.append(furigana)
+
+        # Obtenemos las definiciones
+        definitions = word.findAll('span', {'class': 'meaning-meaning'})
+
+        i = 1
+        for definition in definitions:
+            definition = remove_tags(definition)
+            if not ('【' in definition):
+                element.append(str(i) + '.-' + definition)
+                i = i + 1
+
+        result.append(element)
+
+    return result
---
+++ root.py
@@ -0,0 +1,38 @@
+import functions
+from bs4 import BeautifulSoup
+from pandas import DataFrame
+
+# Insertamos la url
+# N5: 33 páginas
+# N4: 29 páginas
+# N3: 89 páginas
+# N2: 91 páginas
+# N1: 173 páginas
+# Parámetros
+level = 1
+n_pages = 173
+
+
+final_result = []
+index = 1
+for i in range(n_pages):
+    url = 'https://jisho.org/search/%23words%23jlpt-n' + str(level) + '?page=' + str(i+1)
+
+    html = functions.obtain_html(url)
+    bs = BeautifulSoup(html, 'lxml')
+
+    # Obtenemos todas las palabras
+    words = bs.findAll('div', {'class':'concept_light clearfix'})
+
+    # Obtenemos la lista de términos
+    page = functions.obtain_page(words, level, index)
+    index = index + len(words)
+
+    # Lo añadimos a la lista final
+    final_result = final_result + page
+
+print(final_result)
+print(len(final_result))
+
+document = DataFrame.from_records(final_result)
+document.to_csv('N' + str(level) + '.txt', index=False, sep='\t')
